614dec19c888e69d3da25f1c205fa6f7888eb47b
Yan Levit <yan2045@gmail.com>
2023-07-28 18:19:06 +0300

    database set up and post fixes

--- api/db.js
+++ api/db.js
@@ -1,8 +1,11 @@
 import mysql from "mysql"
+import dotenv from "dotenv"
+
+dotenv.config();
 
 export const db = mysql.createConnection({
-  host:"localhost",
-  user:"root",
+  host:"blog.cwv83wd9elvf.eu-central-1.rds.amazonaws.com",
+  user:"admin",
   password: process.env.DB_KEY,
   database:"blog"
 })
