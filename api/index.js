ed409bb737c74706758585bffe18cc72a0cb7a93
Yan Levit <yan2045@gmail.com>
2023-08-18 14:35:48 +0300

    fix

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
 const app = express();
+app.set('trust proxy', 1);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
