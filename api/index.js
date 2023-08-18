a46dbb6bd4d164b69aa05c34ca22141381634fac
Yan Levit <yan2045@gmail.com>
2023-08-18 14:37:56 +0300

    revert

--- api/index.js
+++ api/index.js
@@ -5,7 +5,6 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
 const app = express();
-app.set('trust proxy', 1);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
